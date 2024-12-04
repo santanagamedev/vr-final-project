25b996d4aace6b39c113ec0cc294fed1fa5cdc0d
David Santiago Rojas Páez <santiagodrp37@gmail.com>
2024-12-04 11:36:15 -0500

    Correcion del prefabs relic y creacione de scrips de control de puzzle

---
+++ Assets/AssetsScene/Scripts/PuzzleRelicController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleRelicController : MonoBehaviour
+{
+     public GameObject[] respawns;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void SearchRelicOn()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
