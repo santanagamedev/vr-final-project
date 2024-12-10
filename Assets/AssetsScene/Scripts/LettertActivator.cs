62734b514f1648b08681d2b3e3d9c752d3203ae8
LuisCárdenas <luacardenasgo@unal.edu.co>
2024-12-10 16:13:49 -0500

    Revert "Teleport Integration complete"
    This reverts commit 6183e0c52f74f61fa1ca26dfe1165aaba41ee247.

--- Assets/AssetsScene/Scripts/LettertActivator.cs
+++ Assets/AssetsScene/Scripts/LettertActivator.cs
@@ -2,42 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LettertActivator : MonoBehaviour {
+public class LettertActivator : MonoBehaviour
+{
     Animator animator;
     AudioSource clip;
-
-    private bool isLetterActive = false;
-
-    [SerializeField] private LayerMask playerLayer; // Layer for the player object
-    [SerializeField] private float detectionRadius = 2.0f; // Radius of the detection zone
-
-
     private void Start() {
         animator = GetComponent<Animator>();
         clip = GetComponent<AudioSource>();
     }
-
     private void OnTriggerEnter(Collider other) {
-        if (!isLetterActive) {
-            if (other.gameObject.CompareTag("Player")) {
-                animator.SetBool("PlayerOnRange", true);
-                gameObject.GetComponent<Collider>().enabled = false;
-                clip.PlayOneShot(clip.clip);
-
-                isLetterActive = true;
-            }
-        }
-    }
-
-    private void OnTriggerStay(Collider other) {
-        if (!isLetterActive) {
-            if (other.gameObject.CompareTag("Player")) {
-                animator.SetBool("PlayerOnRange", true);
-                gameObject.GetComponent<Collider>().enabled = false;
-                clip.PlayOneShot(clip.clip);
-
-                isLetterActive = true;
-            }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("PlayerOnRange", true);    
+            gameObject.GetComponent<Collider>().enabled = false;
+            clip.PlayOneShot(clip.clip);
         }
     }
 }
